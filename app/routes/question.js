8834d7ae0d50a7ef91eb0420595016eb46447293
John Watson <john.watson1@defra.gov.uk>
2024-01-16 15:21:54 +0000

    Add service bus

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,3 +1,5 @@
+const { MessageSender } = require('ffc-messaging')
+
 module.exports = [{
   method: 'GET',
   path: '/question',
@@ -11,8 +13,22 @@ module.exports = [{
   method: 'POST',
   path: '/question',
   options: {
-    handler: (request, h) => {
+    handler: async (request, h) => {
       console.log(request.payload)
+      try {
+        const sender = new MessageSender({
+          useCredentialChain: false,
+          host: process.env.MESSAGE_HOST,
+          username: process.env.MESSAGE_USER,
+          password: process.env.MESSAGE_PASSWORD,
+          address: 'ffc-ffd-data'
+        })
+        await sender.sendMessage({ body: request.payload, type: 'question', source: 'ffc-ffd-frontend-poc' })
+        await sender.closeConnection()
+      } catch (error) {
+        console.log('error', error)
+        throw error
+      }
       return h.redirect('confirmation')
     }
   }
